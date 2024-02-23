030364ea72778034e68d1aedfc9d06153f8df462
unknown <camendez632@ucatolica.edu.co>
2024-02-23 18:28:48 -0500

    primercommit

---
+++ src/main/java/com/mycompany/clienteservidor/ClienteServidor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clienteservidor;
+
+/**
+ *
+ * @author asus
+ */
+public class ClienteServidor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
